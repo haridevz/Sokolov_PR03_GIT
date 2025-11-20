e5ee5e7fc1b6b30160ac55145da1817ac7170c66
Solopina <katya-solo@list.ru>
2025-11-20 11:01:05 +0300

    Add button in form with date and time message

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Sokolov_PR03_GIT
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Время и дата: {DateTime.Now}");
+        }
     }
 }
